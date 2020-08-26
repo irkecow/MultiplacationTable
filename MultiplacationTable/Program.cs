bae3e2940a7aa2ab961c0dea39b5213f7ca4d9e5
Gavin collier <gavin.mc.collier@gmail.com>
2020-08-26 12:16:52 -0400

    added basic program

---
+++ MultiplacationTable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Data;
+
+namespace MultiplacationTable
+{
+    class Program
+    {
+        static void Main()
+        {
+            string row = "";
+
+            for (int i = 1; i < 10; i++)
+            {
+                for(int j = 1; j < 10; j++)
+                {
+
+                   row = row + "|" + (i * j);
+                }
+                
+                Console.WriteLine(row,"\n");
+                row = "";
+            }
+        }
+    }
+}

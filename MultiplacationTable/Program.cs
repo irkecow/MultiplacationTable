c95af83acd25e19948646caad25ce39de71e7cd3
Gavin collier <gavin.mc.collier@gmail.com>
2020-08-27 15:19:12 -0400

    added lines to the top and bottom of the table allowed the user to choose table size

--- MultiplacationTable/Program.cs
+++ MultiplacationTable/Program.cs
@@ -1,21 +1,50 @@
 ﻿using System;
 using System.Collections.Specialized;
 using System.Data;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace MultiplacationTable
 {
     class Program
     {
+        static void WriteBar(int legnth)
+        {
+            string line = "+";
+        
+            for (int i = 0; i < legnth * 5 - 1; i++) {
+                line = line + "-";
+            }
+
+            line = line + "+";
+
+            Console.WriteLine(line);
+            
+        }
         static void Main()
         {
             string row = "";
 
-            for (int i = 1; i < 10; i++)
+            Console.WriteLine("What number should the table go up to?\n");
+            string maxNumberString = Console.ReadLine();
+
+            int maxNumber = Int32.Parse(maxNumberString);
+
+            int LineLegnth = maxNumber;
+
+            Console.WriteLine("\n\n");
+
+            WriteBar(LineLegnth);
+
+            maxNumber = maxNumber + 1;
+
+            for (int i = 1; i < maxNumber; i++)
             {
-                for(int j = 1; j < 10; j++)
+                for(int j = 1; j < maxNumber; j++)
                 {
-                   if (i * j < 10) { row = row + "| " + (i * j); }
-                   else { row = row + "|" + (i * j); }                  
+                    if (i * j < 10) { row = row + "|   " + (i * j); }
+                    else if (i * j < 100) { row = row + "|  " + (i * j); }
+                    else if (i * j < 1000) { row = row + "| " + (i * j); }
+                    else { row = row + "| " + (i * j); }
                 }
 
                 row = row + "|";
@@ -23,6 +52,7 @@ namespace MultiplacationTable
               
                 row = "";
             }
+            WriteBar(LineLegnth);
         }
     }
 }
